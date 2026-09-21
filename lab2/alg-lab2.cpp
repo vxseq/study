3ca4d3373868d7d72edbcac9485161dfce1a5006
vxseq <ozb7837@gmail.com>
2026-09-21 12:33:04 +0300

    lab2

--- lab2/alg-lab2.cpp
+++ lab2/alg-lab2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void base()
 {
     double hour;
-    cout << "Введите количество часов";
+    cout << "Введите количество часов" << "\n";
     cin >> hour;
     if (cin.fail() || hour<0)
     {
@@ -15,14 +15,14 @@ void base()
         return;
     }
     int minutes{static_cast<int>(hour*60)};
-    cout << "Количество минут = " << minutes;
+    cout << "Количество минут = " << minutes << "\n";
 }
 
 void medium()
 {
     double pi{3.1415};
     double R,h;
-    cout << "Введите радиус и высоту";
+    cout << "Введите радиус и высоту" << "\n";
     cin >> R >> h;
     if (cin.fail() || h<=0 || R<=0)
     {
@@ -31,7 +31,7 @@ void medium()
         cin.ignore(10000,'\n');
         return;
     }
-    double V{pi*(R*R)*h};
+    double V{pi*static_cast<double>(R*R)*h};
     cout << "Объем цилиндра = " << V << "\n";
 }
 
@@ -39,7 +39,17 @@ void advanced()
 {
     double x;
     int y;
-    cout << "Введите числа x и y"
+    cout << "Введите числа x и y" << "\n";
+    cin >> x >> y;
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(10000,'\n');
+        cout << "Неверный тип данных, введите числа";
+        return;
+    }
+    double result{pow(x,static_cast<double>(y))};
+    cout << "Результат: " << result << "\n";
 }
 
 int main(){
@@ -51,7 +61,7 @@ int main(){
         cout << "Неверный тип данных";
         cin.clear();
         cin.ignore(10000,'\n');
-        return;
+        //return;
     }
     if (task_level==1)
     {
@@ -67,7 +77,7 @@ int main(){
     }
     else
     {
-        cout << "Неверный ввод, введите 1 или 2 или 3";
+        cout << "Неверный ввод, введите 1 или 2 или 3" << "\n";
     }
     return 0;
 }
